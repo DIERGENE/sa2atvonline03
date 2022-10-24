63ac409311a34274b67c03bb872888688bdc8144
DIERGENE <diergene@gmail.com>
2022-10-24 15:57:30 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval( function() {
+    nextImage();
+},5000)
+
+function nextImage(){
+    count++;
+    if(count>4){
+        count = 1;
+    }
+
+document.getElementById("radio"+count).checked = true;
+}
+
+//..login..//
+
+function toggleResetPswd(e){
+    e.preventDefault();
+    $('#logreg-forms .form-signin').toggle() // display:block or none
+    $('#logreg-forms .form-reset').toggle() // display:block or none
+}
+
+function toggleSignUp(e){
+    e.preventDefault();
+    $('#logreg-forms .form-signin').toggle(); // display:block or none
+    $('#logreg-forms .form-signup').toggle(); // display:block or none
+}
+
+$(()=>{
+    // Login Register Form
+    $('#logreg-forms #forgot_pswd').click(toggleResetPswd);
+    $('#logreg-forms #cancel_reset').click(toggleResetPswd);
+    $('#logreg-forms #btn-signup').click(toggleSignUp);
+    $('#logreg-forms #cancel_signup').click(toggleSignUp);
+})
